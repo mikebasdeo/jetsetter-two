1b78750d4d50ffc313ac8177381b87062c23ccf9
Mike Basdeo <mikebasdeo@gmail.com>
2020-09-20 13:01:08 -0400

    still working

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import Application from './components/Application';
 
 import './index.css';
+console.log('heartbeat')
 
 ReactDOM.render(<Application />, document.getElementById('root'));
